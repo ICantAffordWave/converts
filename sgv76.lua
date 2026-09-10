3b9a8585d07bac437fe49f903a407447f6246a8b
ICantAffordWave <tomasxd126@gmail.com>
2026-09-10 16:59:08 -0300

    Update sgv76.lua

--- sgv76.lua
+++ sgv76.lua
@@ -1244,8 +1244,7 @@ function GetAccessory(n, d)
             local h = v:WaitForChild("Handle")
             local mesh = h:FindFirstChildOfClass("SpecialMesh")
 
-            if (mesh and hats_1[d].AssetId == mesh.MeshId)
-                or (h:IsA("MeshPart") and hats_1[d].AssetId == h.MeshId) then
+            if (mesh and hats_1[d].AssetId == mesh.MeshId) or (h:IsA("MeshPart") and hats_1[d].AssetId == h.MeshId) then
                 return h
             end
         end
